849a7a9345d8e3b0ea028977981b2953cf546bdb
eteubert <eric@ericteubert.de>
2016-12-29 13:37:36 +0800

    implement

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+// when run in a browser, we can use the native DOMParser, but node doesn't have it
+if (typeof window === 'undefined' || typeof window.DOMParser === 'undefined') {
+    var DOMParser = require('xmldom').DOMParser;
+}
+
+var npt = require('normalplaytime');
+
 var parse = function(text) {
 
-    return [];
+    var parser = new DOMParser();
+    var xml    = parser.parseFromString(text, "text/xml");
+    var chapterTags = xml.getElementsByTagNameNS('http://podlove.org/simple-chapters', 'chapter');
+
+    var chapters = [];
+
+    for (var i = 0; i < chapterTags.length; i++) {
+        var start = npt.parse(chapterTags[i].getAttribute('start'));
+        var title = chapterTags[i].getAttribute('title').trim();
+
+        if (start !== null) {
+            var chapter = {
+                start: start,
+                title: title
+            };
+            chapters.push(chapter);
+        }
+    }
+
+    return chapters;
 
 };
 
--- test/test.js
+++ test/test.js
@@ -1,11 +1,40 @@
 var assert = require("assert");
 var psc = require('../index.js');
 
+const wrap = function(xml) {
+    return '<psc:chapters xmlns:psc="http://podlove.org/simple-chapters" version="1.2">' + xml + '</psc:chapters>';
+}
+
+const chapter_json = { start: 1200, title: "Intro" };
+
 describe('psc', function() {
   describe('#parse()', function() {
     
     it('should return empty list for invalid strings', function() {
       assert.deepEqual([], psc.parse("abc"));
+    });
+
+    it('should return json with start and title', function() {
+      assert.deepEqual([chapter_json], psc.parse(wrap('<psc:chapter title="Intro" start="00:00:01.200"/>')));
+    });
+
+    it('should return trim the title', function() {
+      assert.deepEqual([chapter_json], psc.parse(wrap('<psc:chapter title=" Intro " start="00:00:01.200"/>')));
+    });
+
+    it('should accept emoji 💩 in the title', function() {
+      assert.deepEqual([{start: 1200, title: "Intro 💩"}], psc.parse(wrap('<psc:chapter title="Intro 💩" start="00:00:01.200"/>')));
+    });
+
+    it('should skip chapters with invalid timecode', function() {
+      assert.deepEqual([chapter_json], psc.parse(wrap('<psc:chapter title="Intro" start="00:00:01.200"/><psc:chapter title="Second" start="a00:00:01.200"/>')));
+    });
+
+    it('should read multiple chapters', function() {
+      assert.deepEqual([
+        { start: 1200, title: "Intro" },
+        { start: 2400, title: "Second" }
+      ], psc.parse(wrap('<psc:chapter title="Intro" start="00:00:01.200"/><psc:chapter title="Second" start="00:00:02.400"/>')));
     });
 
   });
